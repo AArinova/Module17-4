b6c8c0bf0d239dd7177287d03b205860c33268c4
AArinova <aliya-arinova@yandex.ru>
2025-01-12 19:54:49 +0700

    Add files via upload

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,1 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import Column, Integer, String
+
+
+engine = create_engine("sqlite:///taskmanager.db", echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
 
